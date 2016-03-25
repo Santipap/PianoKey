bb94d1ba617877c7bf8c537d7f104d9d84dca9d9
Santipap <hibiki307@gmail.com>
2016-03-26 05:52:19 +0700

    Piano compleate chang song key after day

--- app/src/main/java/ice_pbru/netkasak/santipap/piano/MainActivity.java
+++ app/src/main/java/ice_pbru/netkasak/santipap/piano/MainActivity.java
@@ -7,31 +7,141 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mpBgm;
+    Button btnOK;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnOK = (Button) findViewById(R.id.c1);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MediaPlayer mpEffect
+                        = MediaPlayer.create(MainActivity.this, R.raw.song1);
+                mpEffect.start();
+                mpEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+            }
+        });
+
+
+        btnOK = (Button) findViewById(R.id.c2);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MediaPlayer mpEffect
+                        = MediaPlayer.create(MainActivity.this, R.raw.song2);
+                mpEffect.start();
+                mpEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+            }
+        });
+
+
+        btnOK = (Button) findViewById(R.id.c3);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MediaPlayer mpEffect
+                        = MediaPlayer.create(MainActivity.this, R.raw.song3);
+                mpEffect.start();
+                mpEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+            }
+        });
+
+
+        btnOK = (Button) findViewById(R.id.c4);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MediaPlayer mpEffect
+                        = MediaPlayer.create(MainActivity.this, R.raw.song4);
+                mpEffect.start();
+                mpEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+            }
+        });
+
+
+        btnOK = (Button) findViewById(R.id.c5);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MediaPlayer mpEffect
+                        = MediaPlayer.create(MainActivity.this, R.raw.song5);
+                mpEffect.start();
+                mpEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+            }
+        });
+
+
+        btnOK = (Button) findViewById(R.id.c6);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MediaPlayer mpEffect
+                        = MediaPlayer.create(MainActivity.this, R.raw.song6);
+                mpEffect.start();
+                mpEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+            }
+        });
+
+
+        btnOK = (Button) findViewById(R.id.c7);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MediaPlayer mpEffect
+                        = MediaPlayer.create(MainActivity.this, R.raw.song7);
+                mpEffect.start();
+                mpEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+            }
+        });
+
+
+        btnOK = (Button) findViewById(R.id.c8);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                MediaPlayer mpEffect
+                        = MediaPlayer.create(MainActivity.this, R.raw.song8);
+                mpEffect.start();
+                mpEffect.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+                    }
+                });
+            }
+        });
+
+
+
     }
-    private void _intObject()
-    {
-        Button c_song = (Button) this.findViewById(R.id.c_song);
-        c_song.setOnClickListener(new View.OnClickListener() {
-                                      public void onClick(View v)
-
-                                      {
-                                          MediaPlayer play = MediaPlayer.create(getBaseContext(),
-                                                  R.raw.song1);
-                                          play.start();
-
-                                      }
 
 
-                                  }
-        );
 
 
-    }
 
 
 }
