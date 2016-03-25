8c82d2a1cea9384b37fac036ff2248be07b633c4
Santipap <hibiki307@gmail.com>
2016-03-26 04:58:22 +0700

    Insert song not good T T

--- app/src/main/java/ice_pbru/netkasak/santipap/piano/MainActivity.java
+++ app/src/main/java/ice_pbru/netkasak/santipap/piano/MainActivity.java
@@ -1,7 +1,9 @@
 package ice_pbru.netkasak.santipap.piano;
-
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    private void _intObject()
+    {
+        Button c_song = (Button) this.findViewById(R.id.c_song);
+        c_song.setOnClickListener(new View.OnClickListener() {
+                                      public void onClick(View v)
+
+                                      {
+                                          MediaPlayer play = MediaPlayer.create(getBaseContext(),
+                                                  R.raw.song1);
+                                          play.start();
+
+                                      }
+
+
+                                  }
+        );
+
+
+    }
+
+
 }
